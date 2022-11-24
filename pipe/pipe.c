0c01b3a869070c1cac2a6ecdf45a98e1bfee3fd0
dachoa1005 <hoa.pd185357@sis.hust.edu.vn>
2022-11-24 00:16:44 -0800

    update

--- pipe/pipe.c
+++ pipe/pipe.c
@@ -11,10 +11,11 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main(void) {
-  int pfds[2];
+  int pfds[2]; 
   int SIZE = 30;
   char buf[SIZE];
   char * msg = "This is a pipe example!";
@@ -26,16 +27,17 @@ int main(void) {
   if (!fork()) {
 
     printf(" CHILD: writing to pipe\n");
-    write(pfds[1], msg, SIZE);
+    write(pfds[1], msg, SIZE); // pdfs[1] to write to pipe
     printf(" CHILD: exiting\n");
-
+    printf("PID of child: %d\n", getpid());;
   /** reader **/
   }else{
+    wait(NULL); // wait for child to exit
 
     printf("PARENT: reading from pipe\n");
-    read(pfds[0], buf, SIZE);
+    read(pfds[0], buf, SIZE); // pdfs[0] to read from pipe
     printf("PARENT: read \"%s\"\n", buf);
-    wait(NULL);
+    printf("PID of parent: %d\n", getpid());
   }
 
   return 0;
